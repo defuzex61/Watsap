2a8c496d8be728c77a4930b65538175741e9b9c0
defuzex61 <isip_r.r.korniyk@mpt.ru>
2023-09-24 22:13:29 +0300

    OAO Plotnie Turboyaitsa

---
+++ Watsap/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Watsap
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void newChatBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NewChatWindow newChat = new NewChatWindow();
+            newChat.Show();
+        }
+
+        private void oldChatBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
+ 
